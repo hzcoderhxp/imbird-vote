85382b3e33e57b44d9541eef9678e5271d8960ee
huxiaopan <181602843@qq.com>
2018-02-09 15:55:39 +0800

    remove innerport

--- src/main/java/com/stone/config/CommonConfig.java
+++ src/main/java/com/stone/config/CommonConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
-    @Value("${server.inner.port}")
+    @Value("${server.port}")
     private Integer port;
 
     @Bean
