8bf3edecbe2260e01005a0ed5eb2fd0d75270163
huxiaopan <181602843@qq.com>
2018-02-09 16:28:08 +0800

    s

--- src/main/java/com/stone/config/CommonConfig.java
+++ src/main/java/com/stone/config/CommonConfig.java
@@ -1,6 +1,5 @@
 package com.stone.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +11,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
-    @Value("${server.inner.port}")
-    private Integer port;
 
     @Bean
     public EmbeddedServletContainerFactory embeddedServletContainerFactory(){
         UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
-        factory.addBuilderCustomizers(builder -> builder.addHttpListener(port,"0.0.0.0"));
+        //factory.addBuilderCustomizers(builder -> builder.addHttpListener(port,"0.0.0.0"));
         return factory;
     }
 
